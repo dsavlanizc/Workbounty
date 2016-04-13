7cc8906f93203f5d076cf385379e7204308ac494
pacharyazc <pacharya@zerochaos.com>
2016-04-13 12:21:15 +0530

    View Profile

--- Workbounty/Models/TeamInformation.cs
+++ Workbounty/Models/TeamInformation.cs
@@ -24,5 +24,18 @@ namespace Workbounty.Models
         public string FirstName { get; set; }
         public int PublishedTo { get; set; }
     }
+
+    public class UserProfileInfo
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public System.DateTime DateOfBirth { get; set; }
+        public string PhoneNumber { get; set; }
+        public string InterestedKeywords { get; set; }
+        
+
+    }
 }
      
